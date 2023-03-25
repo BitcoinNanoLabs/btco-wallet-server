a38da164bcde08a172927274cda520f30b294982
douglasbarros <dbs.douglas@gmail.com>
2023-03-25 13:20:37 -0300

    Update infinitumcast.py

--- infinitumcast.py
+++ infinitumcast.py
@@ -611,7 +611,7 @@ async def init_app():
         """Open redis connections"""
         log.server_logger.info("Opening redis connections")
         app['rdata'] = await aioredis.from_url("redis://{0}:{1}".format(redis_host, str(redis_port)),
-                                                db=int(os.getenv('REDIS_DB', '2')), encoding='utf-8', minsize=2, maxsize=15)
+                                                db=int(os.getenv('REDIS_DB', '2')), encoding='utf-8')
         # Global vars
         app['clients'] = {} # Keep track of connected clients
         app['last_msg'] = {} # Last time a client has sent a message
